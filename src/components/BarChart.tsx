51b4927fe2d8533fb7d87ba02f6eee4258304284
Abdul Rehman <ar479720@gmail.com>
2025-01-19 11:17:53 +0500

    Refactor BarChart component

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -3,8 +3,7 @@ import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
 import { mockBarData as data } from "../data/mockupdata";
 
-const BarChart = () => {
-  const isDashboard = true; // added
+const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
